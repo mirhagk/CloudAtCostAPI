60f764560fe003d5f16a0cee8eb657fcbef99cb0
Nathan Jervis <mirhagk@gmail.com>
2015-03-15 17:23:00 -0400

    Added credential loading

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-namespace Console
+namespace CloudAtCostAPIConsole
 {
     /// <summary>
     /// The point of this project is to wrap the library in a command line application
@@ -15,13 +15,63 @@ namespace Console
     class Program
     {
         static string CredentialFile { get; set; }
+        static string Key { get; set; }
+        static string Login { get; set; }
         static void Main(string[] args)
         {
-            for(int i=0;i<args.Length;i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 if (args[i].StartsWith("--"))
                 {
-
+                    switch (args[i].Substring(2).ToLowerInvariant())
+                    {
+                    }
+                }
+                else if (args[i].StartsWith("-"))
+                {
+                    var param = args[i].Substring(1).ToLowerInvariant();
+                    if (i + 1 == args.Length)
+                    {
+                        Console.Error.WriteLine("Expected value for parameter {0}", param);
+                        return;
+                    }
+                    var value = args[++i].ToLowerInvariant();
+                    switch (param)
+                    {
+                        case "c": goto case "cred";
+                        case "cred": goto case "credentials";
+                        case "credentials":
+                            CredentialFile = value;
+                            break;
+                        case "k": goto case "key";
+                        case "key":
+                            Key = value;
+                            break;
+                        case "l": goto case "login";
+                        case "login":
+                            Login = value;
+                            break;
+                        default:
+                            Console.Error.WriteLine("Could not understand parameter {0}", param);
+                            return;
+                    }
+                }
+            }
+            if (CredentialFile != null)
+            {
+                if (Key != null || Login != null)
+                {
+                    Console.Error.WriteLine("Can't specify both the credentials and the file");
+                    return;
+                }
+                foreach (var line in File.ReadAllLines(CredentialFile).Select(x => x.Split('=').Select(y => y.Trim().ToLowerInvariant()).ToArray()))
+                {
+                    switch (line[0])
+                    {
+                        case "key":Key = line[1]; break;
+                        case "login":Login = line[1]; break;
+                        default: Console.Error.WriteLine("Don't understand key {0}", line[0]); break;
+                    }
                 }
             }
             var client = new CloudAtCostAPI.CloudClient();
